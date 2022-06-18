5b79703bf876352053fdf611ab087630a47b264a
Vishnu vardhan D K <dkvishnuvardhan94@gmail.com>
2022-06-18 10:28:16 +0530

    updated

--- shopbop-clone/src/App.jsx
+++ shopbop-clone/src/App.jsx
@@ -3,15 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 import {Navbar} from "./components/Navbar"
 import {Footer} from "./components/Footer"
-import { Homepage } from './pages/HomePage'
-import Accessories from "./components/Accessories"
-import Bags from "./components/Bags"
-import Mens from "./components/Mens"
-import Womens from "./components/Womens"
-import {SimpleCard} from "./pages/SignIn"
-import SignupCard from "./pages/SignUp"
-import {Cart} from "./pages/Cart"
-import {Payment} from "./pages/Payment"
+
 import { AllRoutes } from './pages/AllRoutes'
 
 
@@ -21,15 +13,6 @@ function App() {
   return (
     <div className="App">
       <Navbar/> 
-      {/* <SignupCard/> */}
-      {/* <SimpleCard/> */}
-      {/* <Homepage/> */}
-      {/* <Accessories/> */}
-      {/* <Bags/> */}
-      {/* <Mens/> */}
-      {/* <Womens/> */}
-      {/* <Cart/> */}
-      {/* <Payment/> */}
       <AllRoutes/>
       <Footer/>
     </div>
